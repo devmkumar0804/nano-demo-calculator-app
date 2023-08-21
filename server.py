c47610a87e4aa9354dd05bf16641e115a6ef9720
devmkumar0804 <73732254+devmkumar0804@users.noreply.github.com>
2023-08-21 22:06:01 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,12 +8,12 @@ def greeting():
     return ''
 
 @app.route("/calculator/add", methods=['POST'])
-def add():
-    return ''
+def add(a,b):
+    return a+b
 
 @app.route("/calculator/subtract", methods=['POST'])
-def subtract():
-    return ''
+def subtract(a,b):
+    return a-b
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
